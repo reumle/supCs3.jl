8f16d66970d0c0032b82e19dfcf6e2885334e5de
elm <lemeurmail@gmail.com>
2020-02-22 08:42:20 +0100

    update files /3.3. supCs3 passes (many/most?) tests
    #### MOI_wrapper.jl
    adapt to raw_status(sol.statusStr) instead of sol.info

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -431,7 +431,7 @@ function MOI.optimize!(optimizer::Optimizer)
     objective_value = (optimizer.maxsense ? -1 : 1) * sol.info.pobj
     dual_objective_value = (optimizer.maxsense ? -1 : 1) * sol.info.dobj
     solve_time = (sol.info.setupTime + sol.info.solveTime) / 1000
-    optimizer.sol = MOISolution(ret_val, raw_status(sol.info), primal, dual,
+    optimizer.sol = MOISolution(ret_val, raw_status(sol.statusStr), primal, dual,
                                 slack, objective_value, dual_objective_value,
                                 objective_constant, solve_time, sol.info.iter)
 end
